3988da56dd3958fa38a6bd3f155fc4bf12d8d4a2
Rafet <rafetr@risk.az>
2022-12-10 10:21:07 +0400

    init

---
+++ HelloWorldTask/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace HelloWorldTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1,number2;
+            number1 = Convert.ToInt32(Console.ReadLine());
+            number2 = Convert.ToInt32(Console.ReadLine());
+            int sum = number1 + number2;
+            Console.WriteLine(number1 + "+" + number2 + "=" +  sum);
+            Console.WriteLine($"{number1}+{number2}={sum}");
+            //number1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Hello, World!");
+        }
+    }
+}
